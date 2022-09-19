3fc737021b0ae8af6eac2a2e1b7d1387d5160737
mnwuPark <mw.park@geeksbin.com>
2022-09-19 10:46:11 +0900

    fixed 1. add range 20 for string 2. add input, output

--- mw.park/HDMI.py
+++ mw.park/HDMI.py
@@ -6,7 +6,23 @@ def input_print(value):
     input_string = ""
     for i in value:
         a = i
-        
+
     f_p = int((a / 20) + 1)
     t_p = a % 20
     print_list.append([f_p, t_p])
+
+    for i in range(20):
+        i += 1
+        j = int(i / 20) + 1
+        k = i % 20
+        f = [j, k]
+        if f in print_list:
+            input_string += "■"
+            if i % 20 == 0:
+                print(input_string)
+                input_string = ""
+        else:
+            input_string += "□"
+            if i % 20 == 0:
+                print(input_string)
+                input_string = ""
