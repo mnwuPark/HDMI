14f906a73fc224074fded596a4603ac1c6f1c1ac
mnwuPark <mw.park@geeksbin.com>
2022-09-19 10:31:04 +0900

    Added

---
+++ mw.park/HDMI.py
@@ -0,0 +1,13 @@
+print_list = []
+def input_print(value):
+    count = value.__len__()
+    length = count * 3
+    side = length + 10
+    input_string = ""
+    for i in value:
+        a = i
+    
+    f_p = int((a / 20) + 1)
+    t_p = a % 20
+    print_list.append([f_p, t_p])
+
